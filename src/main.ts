327c67ab3aa602ab8de9866846cd167b4c723533
marekhoryna <marek.horyna@outlook.com>
2019-01-17 15:34:00 +0100

    fixed import

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { environment } from './environments/environment';
-import { EntryModule } from './entry/entry.module';
+import { ComponentsModule } from './components/components.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(EntryModule)
+platformBrowserDynamic().bootstrapModule(ComponentsModule)
   .catch(err => console.error(err));
