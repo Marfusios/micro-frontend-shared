a22cd3cf670ac08d7d7ac08e63f221d4af6c8e3b
marekhoryna <marek.horyna@outlook.com>
2019-01-21 15:02:42 +0100

    fixed references

--- src/components/app-version/app-version.component.ts
+++ src/components/app-version/app-version.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-version',
-  templateUrl: './mfs-version.component.html',
-  styleUrls: ['./mfs-version.component.scss']
+  templateUrl: './app-version.component.html',
+  styleUrls: ['./app-version.component.scss']
 })
 export class AppVersionComponent { }
