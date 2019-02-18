c126276897fe5fd2ca02a6465e9e525ac18531cf
Mariusz Kotas <m@mkotas.cz>
2019-02-18 16:16:22 +0100

    Version service - make it stateful

--- src/services/version-service/version.service.ts
+++ src/services/version-service/version.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class VersionService {
+
+  public called = 0;
+  public lastService = 'none';
+
   public getVersion(): string {
     return '2.1';
+  }
+
+  public registerService(serviceName: string) {
+    this.lastService = serviceName;
+    this.called++;
   }
 }
